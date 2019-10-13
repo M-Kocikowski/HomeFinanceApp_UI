13654962084b71908a49b71af4ebf635f740bc62
M-Kocikowski <marcin.kocikowski@gmail.com>
2019-10-13 16:49:29 +0200

    xml file upload

---
+++ components/FileUpload.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <b-form-file
+      v-model="file"
+      :state="Boolean(file)"
+      placeholder="Choose a file or drop it here..."
+      drop-placeholder="Drop file here..."></b-form-file>
+    <div class="mt-3">Selected file: {{ file ? file.name : '' }}</div>
+    <b-button :disabled="buttonDisabled" type="button" variant="secondary" @click="post">Send File</b-button>
+    <b-table :items="items"></b-table>
+  </div>
+</template>
+
+<script>
+
+    export default {
+        data() {
+            return {
+                file: null,
+                items: []
+            }
+        },
+
+        computed: {
+            buttonDisabled: function () {
+                return !this.file;
+            }
+        },
+
+        methods: {
+            async post() {
+
+                if (this.file.type !== 'text/xml') {
+                    alert("Not a valid file type");
+                    return;
+                }
+
+                let formData = new FormData();
+                formData.append('file', this.file);
+                let resp = await this.$axios.post('http://localhost:8080/api/file', formData, {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
+                });
+                this.items = resp.data;
+            }
+        },
+
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/history/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <b-form-group label="Choose input type">
+      <b-form-radio-group v-model="selected" :options="options">
+      </b-form-radio-group>
+    </b-form-group>
+    <FileUpload v-if="selected==='F'"/>
+  </div>
+</template>
+
+<script>
+
+    import FileUpload from "@/components/FileUpload";
+
+    export default {
+        components: {
+            FileUpload
+        },
+
+        data() {
+            return {
+                selected: '',
+                options: [
+                    {text: 'File Upload', value: 'F'},
+                    {text: 'Manual Entry', value: 'M'}
+                ]
+            }
+        },
+
+    }
+</script>
+
+<style scoped>
+
+</style>

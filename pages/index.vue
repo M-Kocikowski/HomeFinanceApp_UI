17f18b0632fb4f210a50b39a06fd84023b5c1955
M-Kocikowski <marcin.kocikowski@gmail.com>
2019-10-14 23:28:48 +0200

    removed components

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,77 @@
 <template>
   <div>
-    <DatePicker/>
-    <AccountTable/>
+    <b-container fluid id="date-picker-container">
+      <b-form @submit="onSubmit" inline>
+        <b-col cols="5">
+          <b-form-group id="form-group-1" label="Date From:" label-for="input-date-from">
+            <b-form-input id="input-date-from" type="date" required v-model="form.fromDate"></b-form-input>
+          </b-form-group>
+        </b-col>
+        <b-col cols="5">
+          <b-form-group id="form-group-2" label="Date To:" label-for="input-date-to">
+            <b-form-input id="input-date-to" type="date" required v-model="form.toDate"></b-form-input>
+          </b-form-group>
+        </b-col>
+        <b-col cols="2">
+          <b-button :disabled="submitDisabled" type="submit" variant="">Show Data</b-button>
+        </b-col>
+      </b-form>
+    </b-container>
+    <b-table bordered small responsive hover :items="items"></b-table>
   </div>
 </template>
 
 <script>
-  import DatePicker from "@/components/DatePicker";
-  import AccountTable from "@/components/AccountTable";
 
-  export default {
-      components:{
-          DatePicker,
-          AccountTable
-      }
-  }
+    export default {
+
+        data() {
+            return {
+                form: {
+                    fromDate: '',
+                    toDate: '',
+                },
+                items: []
+            }
+        },
+
+        methods: {
+            getInitialDates() {
+                this.form.fromDate = this.$moment().subtract(2, "months").format('YYYY-MM-DD');
+                this.form.toDate = this.$moment().format('YYYY-MM-DD');
+            },
+
+            async onSubmit(e) {
+                if (e !== undefined){
+                    e.preventDefault();
+                }
+                let response = await this.$axios.get(`http://localhost:8080/api/operations/${this.form.fromDate}/${this.form.toDate}`);
+                this.items = response.data;
+            },
+
+
+        },
+
+        computed: {
+            submitDisabled: function () {
+                return !(this.form.fromDate !== '' && this.form.toDate !== '' && this.form.fromDate <= this.form.toDate)
+            }
+        },
+
+        beforeMount() {
+            this.getInitialDates();
+            this.onSubmit();
+        },
+
+    }
 
 </script>
+
+<style scoped>
+  #date-picker-container {
+    width: 60%;
+    margin: 30px auto;
+    font-weight: lighter;
+    font-size: larger;
+  }
+</style>

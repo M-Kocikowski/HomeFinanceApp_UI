9d3a42af8622512e93658f2fbece8111c6e86333
M-Kocikowski <marcin.kocikowski@gmail.com>
2019-10-17 19:56:22 +0200

    changed nav bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,9 +2,9 @@
   <div>
     <b-navbar toggleable="lg" type="dark" variant="dark">
       <b-navbar-brand tag="h1" class="mb-1">Home Finance</b-navbar-brand>
-      <b-navbar-nav align="center" small>
-        <b-nav-item :active="link1" @click="clickFirstLink" to="/">Main</b-nav-item>
-        <b-nav-item :active="link2" @click="clickSecondLink" to="/history">Add history</b-nav-item>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item to="/" :active="$route.name == 'index'">Main</b-nav-item>
+        <b-nav-item to="/history" :active="$route.name == 'history'">Add history</b-nav-item>
       </b-navbar-nav>
     </b-navbar>
     <nuxt />
@@ -13,22 +13,7 @@
 
 <script>
   export default {
-      data(){
-          return{
-              link1: true,
-              link2: false
-          }
-      },
-      methods:{
-          clickFirstLink(){
-              this.link1 = true;
-              this.link2 = false;
-          },
-          clickSecondLink(){
-              this.link1 = false;
-              this.link2 = true;
-          }
-      }
+
   }
 </script>
 

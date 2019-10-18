a89691b251a33e09e8fb4fa81cb38c5e966d4af1
M-Kocikowski <marcin.kocikowski@gmail.com>
2019-10-18 22:40:03 +0200

    page for editing single operation

---
+++ pages/history/_id/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <OperationForm
+      :id="item.id"
+      :type="item.type"
+      :orderDate="item.orderDate"
+      :description="item.description"
+      :amount="item.amount"/>
+  </div>
+</template>
+
+<script>
+
+    import OperationForm from "@/components/OperationForm";
+
+    export default {
+
+        components: {
+            OperationForm
+        },
+
+        data() {
+            return {
+                item: {
+                    id: Number,
+                    type: String,
+                    orderDate: String,
+                    description: String,
+                    amount: Number
+                }
+            }
+        },
+
+        async asyncData(context){
+            let id = context.route.params.id;
+            let response = await context.$axios.get(`http://localhost:8080/api/operation/${id}`);
+            return {item: response.data};
+        },
+
+    }
+</script>
+
+<style scoped>
+
+</style>

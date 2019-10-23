fff75a906cf41d31912599b1ad0e7065b7a293d2
M-Kocikowski <marcin.kocikowski@gmail.com>
2019-10-24 00:56:43 +0200

    added init action

--- store/index.js
+++ store/index.js
@@ -3,3 +3,10 @@ export const state = () => ({
 })
 
 export const mutations = {}
+
+export const actions = {
+  async nuxtServerInit(vuexContext, context) {
+    const resp = await context.$axios.get('http://localhost:8080/api/categories');
+    vuexContext.commit('category/categorize', resp.data);
+  }
+}

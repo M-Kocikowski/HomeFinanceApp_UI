18cbb7915c5a7837dd85f3032fbf2ac8394e66b6
M-Kocikowski <marcin.kocikowski@gmail.com>
2019-10-23 23:47:17 +0200

    initialization of categories list

--- store/category.js
+++ store/category.js
@@ -3,7 +3,14 @@ export const state = () => ({
 })
 
 export const mutations = {
-  categorize(state, data){
+  categorize(state, data) {
     state.categories = data;
+  }
+}
+
+export const actions = {
+  async nuxtServerInit(commit, context) {
+    const resp = await context.$axios.get('http://localhost:8080/api/categories');
+    commit('categorize', resp.data);
   }
 }

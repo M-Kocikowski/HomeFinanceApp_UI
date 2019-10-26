c335387fa88b26ec991c640611569504729480e1
M-Kocikowski <marcin.kocikowski@gmail.com>
2019-10-27 01:44:58 +0200

    moved logic from page to component

--- components/AccountTable.vue
+++ components/AccountTable.vue
@@ -1,18 +1,46 @@
 <template>
   <div>
-    <b-table bordered small responsive hover :items="items"></b-table>
+    <b-table
+      bordered
+      small
+      responsive
+      hover
+      :fields="fields"
+      :items="items"
+    >
+      <template v-slot:cell(edit)="row">
+        <b-button size="sm" @click.prevent="redirect(row)">Edit</b-button>
+      </template>
+    </b-table>
   </div>
 </template>
 
 <script>
-    export default {
-        props: {
-          items: []
-        }
+export default {
+  props: {
+    items: Array
+  },
 
-    }
+  data() {
+    return {
+      fields: [
+        { key: "orderDate", label: "Order Date" },
+        { key: "type", label: "Type" },
+        { key: "description", label: "Description" },
+        { key: "category.name", label: "Category" },
+        { key: "amount", label: "Amount" },
+        { key: "edit", label: "Edit" }
+      ]
+    };
+  },
+
+  methods: {
+    redirect(row) {
+      this.$router.push(`/history/${row.item.id}`);
+    },
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
--- components/DatePicker.vue
+++ components/DatePicker.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <b-container fluid id="date-picker-container">
-      <b-form @submit="onSubmit" inline>
+      <b-form @submit.prevent="onSubmit" inline>
         <b-col cols="5">
           <b-form-group id="form-group-1" label="Date From:" label-for="input-date-from">
             <b-form-input id="input-date-from" type="date" required v-model="form.fromDate"></b-form-input>
@@ -13,7 +13,7 @@
           </b-form-group>
         </b-col>
         <b-col cols="2">
-          <b-button :disabled="submitDisabled" type="submit" variant="">Show Data</b-button>
+          <b-button :disabled="submitDisabled" type="submit" variant="info">Show Data</b-button>
         </b-col>
       </b-form>
     </b-container>
@@ -32,13 +32,12 @@
         },
         methods: {
             getInitialDates() {
-                this.form.fromDate = this.$moment().subtract(1, "months").format('YYYY-MM-DD');
+                this.form.fromDate = this.$moment().subtract(2, "months").format('YYYY-MM-DD');
                 this.form.toDate = this.$moment().format('YYYY-MM-DD');
             },
 
             onSubmit(e) {
-                e.preventDefault();
-
+                this.$emit('submitDates', this.form);
             },
 
         },
@@ -58,7 +57,9 @@
 
 <style scoped>
   #date-picker-container {
-    width: 60%;
-    margin: 20px auto;
-  }
+  width: 60%;
+  margin: 30px auto;
+  font-weight: lighter;
+  font-size: larger;
+}
 </style>
